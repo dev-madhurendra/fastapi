b74011f7d687d771794f512b0cfc7a581cd87898
madhuredra <madhuredra.tiwari@zemosolabs.com>
2023-12-13 13:24:59 +0530

    🎨 Formatted the model

--- models-tut/main.py
+++ models-tut/main.py
@@ -16,17 +16,25 @@ class Item(BaseModel):
     # price: float
     # tax: Optional[float] = None
     # tags: List[str] = []
+    
+    
+class ResponseItem(BaseModel):
+    name: str
+    price: float
+    
+    class Config():
+        orm_mode = True
 
 fake_db = []
 
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item) -> Any:
     # Simulate storing the item in a database
-    item_data = item.dict()
+    item_data = item.model_dump()
     fake_db.append(item_data)
     return item
 
-@app.get("/items/", response_model=List[Item])
+@app.get("/items/", response_model=List[ResponseItem])
 async def read_items() -> Any:
     # Simulate getting items from a database
     return fake_db
